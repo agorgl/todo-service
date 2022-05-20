91c5a74eb065a4971866d3695925b8e3f663ce03
Agorgianitis Loukas <agorglouk@gmail.com>
2022-05-20 15:36:00 +0300

    Add todo controller skeleton

---
+++ src/main/java/com/example/todo/controller/TodoController.java
@@ -0,0 +1,14 @@
+package com.example.todo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/todos")
+public class TodoController {
+    @GetMapping
+    public String index() {
+        return "Hello there";
+    }
+}
